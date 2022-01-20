9839179ef46b2ab59efc99f66b07c87d269f7b20
zbynek03 <sev38138@spseol.cz>
2022-01-20 22:01:30 +0100

    20.1

--- src/main.c
+++ src/main.c
@@ -1,57 +1,34 @@
 #include "stm8s.h"
-#include "delay.h"
 #include "milis.h"
 #include "stm8_hd44780.h"
+#include "spse_stm8.h" 
+#include "delay.h"
+#include <stdio.h>
 
-#define LED_Port GPIOC
-#define LED_Pin GPIO_PIN_5
+#define _ISOC99_SOURCE
+#define _GNU_SOURCE
 
-unsigned char bl_state;
-unsigned char data_value;
-unsigned int present_value = 0x0000;
-unsigned int previous_value = 0x0001;
-unsigned int time1 = 0x0010;
-unsigned int difference = 1000;
-
-void GPIO_setup(void);
-void lcd_print(unsigned char x_pos, unsigned char y_pos, unsigned int value);
-
-void main(){
-
-	CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // 16MHz z interního RC oscilátoru
-	init_milis(); 
-    GPIO_setup();
-    lcd_init(); 
-    lcd_clear();
-    lcd_home();
-	lcd_print(0,0,difference);
-
-    while(1){
-							if(GPIO_ReadInputPin(GPIOE,GPIO_PIN_4) == RESET){
-								lcd_print(8,1,milis()/2);
-							}
-							if( milis() != time1){ 
-								lcd_print(8,0,milis()/2);								
-								time1 = milis();			
-							}
-       };
+void setup(void){
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);// taktování na 16MHz
+    lcd_init(); //inicializace LCD
 }
 
 void GPIO_setup(void){
-        GPIO_DeInit(GPIOC);
-        GPIO_Init(GPIOC,((GPIO_Pin_TypeDef)(GPIO_PIN_1 | GPIO_PIN_2)),GPIO_MODE_IN_PU_NO_IT);
-        GPIO_DeInit(GPIOD);
-        GPIO_Init(LED_Port, LED_Pin, GPIO_MODE_OUT_PP_LOW_SLOW);
-	    GPIO_Init(GPIOE,GPIO_PIN_4,GPIO_MODE_IN_FL_NO_IT);
+    GPIO_Init(GPIOC, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_SLOW); //trig
+    GPIO_Init(GPIOC, GPIO_PIN_1, GPIO_MODE_IN_FL_NO_IT); // echo
+		
 }
 
-void lcd_print(unsigned char x_pos, unsigned char y_pos, unsigned int value){
-       char tmp[5] = {0x20, 0x20, 0x20, 0x20, '\0'} ;
-       tmp[0] = ((value / 1000) + 0x30);
-       tmp[1] = (((value / 100) % 10) + 0x30);
-       tmp[2] = (((value / 10) % 10) + 0x30);
-       tmp[3] = ((value % 10) + 0x30);
-       lcd_gotoxy(x_pos, y_pos);
-       lcd_puts(tmp);  
+void main(){
+    setup();
+
+    lcd_gotoxy(0,0);
+    lcd_puts("cum");
+
+    while (1) {
+
+        char text[32];
+
+    }
 }
 #include "__assert__.h"
